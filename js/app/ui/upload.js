06d9dcb7c40021393318bdc8bf197c6cde54beae
Garett Chang <gchang@Garetts-MacBook-Pro-2.local>
2014-11-15 18:03:27 -0500

    Added user model, user controller, configured database.

--- js/app/ui/upload.js
+++ js/app/ui/upload.js
@@ -2,11 +2,31 @@ CYC.ui.ImageUploader = (function($){
 	var thisObj = this;
 	
 	function init() {
-		
+		_bindEvents();
 	}
     
     function uploadImage(imageInfo) {        
         return true;
+    }
+    
+    function _bindEvents() {
+    	// Handle upload button click
+    	$("").click(function () {
+    		$.post(
+	    		CYC.config.baserUrl + "services/listing/upload", 
+	    		JSON.stringify({imageInfo: imageInfo}),
+	    		function() {
+	    			// redirect to listing page from here
+	    			
+	    		}
+			"JSON")
+			.error(function() {
+				// handle failure. maybe display a message
+				alert("An error occured");
+			});  
+		
+    		e.preventDefault();
+    	});
     }
     
     return {
